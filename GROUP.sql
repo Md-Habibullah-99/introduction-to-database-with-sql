c762651d3d9352bd8f5caa17ff50b1da4b665024
md-habibullah-99 <habibullahhisam@gmail.com>
2026-02-25 19:12:31 +0600

    learned something more

--- GROUP.sql
+++ GROUP.sql
@@ -5,5 +5,12 @@ SELECT AVG("book_rating") FROM "rating" WHERE "book_rating" > 4.5 AND "book_rati
 -- GROUP BY
 -- currently is not in that form
 SELECT "book_id", AVG("rating") AS "avarage rating"
-FROM "rating"
+FROM "ratings"
 GROUP BY "book_id";
+
+
+SELECT "book_id", ROUND(AVG("rating"), 2) AS "avarage rating"
+FROM "ratings"
+GROUP BY "book_id"
+--WHERE "avarage rating" > 4.0; --its not possible in group
+HAVING "avarage rating" > 4.0;
