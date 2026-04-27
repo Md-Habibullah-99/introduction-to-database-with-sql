c015a756ab3aca0d08854022129e5acf89accf1c
md-habibullah-99 <habibullahhisam@gmail.com>
2026-04-27 18:20:10 +0600

    Practice again.

--- GROUP.sql
+++ GROUP.sql
@@ -1,16 +1,31 @@
 
-SELECT AVG("book_rating") FROM "rating" WHERE "book_rating" > 4.5 AND "book_rating" < 4.9;
+-- SELECT AVG("gpa") FROM "students" WHERE "gpa" > 4.5 AND "gpa" < 4.9;
 
 
 -- GROUP BY
 -- currently is not in that form
-SELECT "book_id", AVG("rating") AS "avarage rating"
-FROM "ratings"
-GROUP BY "book_id";
+SELECT  group_name AS "Group", AVG(gpa) AS "Avarage gpa of each group"
+FROM students
+GROUP BY group_name;
+
+SELECT  group_name AS "Group", ROUND(AVG(gpa),2) AS "Avarage gpa of each group"
+FROM students
+GROUP BY group_name;
+
+SELECT group_name AS "Group", 
+ (
+  SELECT COUNT(*) 
+  FROM students st
+  WHERE st.group_name=students.group_name
+  AND st.status='Passed'
+) AS "Passed",
+COUNT(*) AS "Group Total"
+FROM students
+GROUP BY group_name;
 
 
-SELECT "book_id", ROUND(AVG("rating"), 2) AS "avarage rating"
-FROM "ratings"
-GROUP BY "book_id"
+-- SELECT "id","roll", ROUND(AVG("rating"), 2) AS "avarage rating"
+-- FROM "ratings"
+-- GROUP BY "book_id"
 --WHERE "avarage rating" > 4.0; --its not possible in group
-HAVING "avarage rating" > 4.0;
+-- HAVING "avarage rating" > 4.0;
