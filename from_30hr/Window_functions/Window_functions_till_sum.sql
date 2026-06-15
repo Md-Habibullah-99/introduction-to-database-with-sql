ed190de527b46953330ea0efe07b9ba8bbaaedac
md-habibullah-99 <habibullahhisam@gmail.com>
2026-06-15 11:38:01 +0600

    Learned window function till aggregate with sum

---
+++ from_30hr/Window_functions/Window_functions_till_sum.sql
@@ -0,0 +1,90 @@
+USE SalesDB;
+
+
+SELECT 
+	o.OrderID ,
+	o.ProductID ,
+	SUM(o.Sales ),
+	o.OrderStatus 
+FROM Sales.Orders o 
+GROUP BY o.OrderID ,
+	o.ProductID ,
+	o.OrderStatus ;
+-- LIMIT OF GROUP BY
+
+
+-- window with agaggregate functions
+
+SELECT
+	o.OrderID ,
+	o.OrderDate ,
+	o.ProductID ,
+	o.OrderStatus ,
+	o.Sales ,
+	SUM(o.Sales) OVER() TotalSales ,
+	SUM(o.Sales) OVER(PARTITION BY o.ProductID ) TotalSalesByProducts ,
+	SUM(o.Sales) OVER (PARTITION BY o.ProductID , o.OrderStatus ) Sales_by_product_and_status
+FROM Sales.Orders o;
+
+-- order by with window (order clause)
+SELECT 
+	o.OrderID ,
+	o.OrderDate ,
+	o.Sales ,
+	RANK() OVER (ORDER BY o.Sales DESC) ranking_by_sales
+FROM Sales.Orders o ;
+
+-- frame clause
+SELECT 
+	o.OrderID ,
+	o.OrderDate ,
+	o.Sales ,
+	SUM(o.Sales )
+	OVER (
+		ORDER BY MONTH(o.OrderDate ) DESC 
+		ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING 
+	) moking_running_total
+FROM Sales.Orders o ;
+
+SELECT 
+	o.OrderID ,
+	o.OrderDate ,
+	o.OrderStatus ,
+	o.Sales ,
+	SUM(o.Sales ) OVER (
+		PARTITION BY o.OrderStatus 
+		ORDER BY o.OrderDate 
+		ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING 
+	) totalSales
+FROM Sales.Orders o ;
+
+--  shortcut only for preceding
+SELECT 
+	o.OrderID ,
+	o.OrderDate ,
+	o.OrderStatus ,
+	o.Sales ,
+	SUM(o.Sales ) OVER (
+		PARTITION BY o.OrderStatus 
+		ORDER BY o.OrderDate 
+		ROWS 2 PRECEDING
+	) totalSales
+FROM Sales.Orders o ;
+
+-- Q: Rank Customers based on their total sales
+SELECT 
+	o.CustomerID ,
+	SUM(o.Sales ) TotalSales ,
+	RANK() OVER (ORDER BY SUM(Sales) DESC ) RankCustomers
+FROM Sales.Orders o 
+GROUP BY o.CustomerID ;
+
+
+--
+SELECT 
+	o.OrderID ,
+	o.ProductID ,
+	o.Sales ,
+	SUM(o.Sales ) OVER() TotalSales ,
+	ROUND(CAST(o.Sales AS FLOAT) /SUM(o.Sales ) OVER() *100 , 2) Parcentage
+FROM Sales.Orders o ;
