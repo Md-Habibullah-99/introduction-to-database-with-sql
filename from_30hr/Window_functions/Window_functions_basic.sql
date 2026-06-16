155f0e2caf37da574417ee17daa2242f3de4bdf6
md-habibullah-99 <habibullahhisam@gmail.com>
2026-06-16 19:25:48 +0600

    Learned basic window fucntions.

--- from_30hr/Window_functions/Window_functions_basic.sql
+++ from_30hr/Window_functions/Window_functions_basic.sql
@@ -80,11 +80,44 @@ FROM Sales.Orders o
 GROUP BY o.CustomerID ;
 
 
---
+-- percentage of contrebution in total salary
 SELECT 
 	o.OrderID ,
 	o.ProductID ,
 	o.Sales ,
 	SUM(o.Sales ) OVER() TotalSales ,
 	ROUND(CAST(o.Sales AS FLOAT) /SUM(o.Sales ) OVER() *100 , 2) Parcentage
+FROM Sales.Orders o ;
+
+----- AVG 
+SELECT 
+	o.OrderID ,
+	o.OrderDate ,
+	o.ProductID ,
+	o.Sales ,
+	AVG(ISNULL(o.Sales , 0)) OVER() avg_all ,
+	AVG(ISNULL(o.Sales , 0)) OVER(PARTITION BY o.ProductID ) avg_product1
+FROM Sales.Orders o;
+
+
+-- greater then average sale
+SELECT *
+FROM (
+	SELECT 
+		o.OrderID ,
+		o.Sales ,
+		AVG(ISNULL(o.Sales , 0)) OVER() avg_all 
+	FROM Sales.Orders o
+) t
+WHERE t.Sales > t.avg_all ;
+
+-- min and max function
+SELECT 
+	o.OrderID ,
+	o.OrderDate ,
+	o.Sales ,
+	MAX(o.Sales ) OVER() HeighestSale ,
+	MIN(o.Sales ) OVER() LowestSale ,
+	MAX(o.Sales ) OVER() - o.Sales  DevitionFromMaximum ,
+	o.Sales - MIN(o.Sales ) OVER() DevitionFromMinimum 
 FROM Sales.Orders o ;
