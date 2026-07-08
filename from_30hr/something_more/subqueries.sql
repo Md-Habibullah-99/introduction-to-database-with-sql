5755dfe3673648ea427f092853b9193fe3226ada
md-habibullah-99 <habibullahhisam@gmail.com>
2026-07-08 23:51:18 +0600

    learn the rest of the topics about sub-queries.

--- from_30hr/something_more/subqueries.sql
+++ from_30hr/something_more/subqueries.sql
@@ -58,6 +58,49 @@ GROUP BY o.CustomerID )t
 	ON t.CustomerID = c.CustomerID ;
 
 
+
+-- with where clause
+SELECT 
+*
+FROM Sales.Employees 
+WHERE Salary < (SELECT AVG(e.Salary ) FROM Sales.Employees e);
+
+
+
+-- witih IN operator
+SELECT 
+*
+FROM Sales.Employees e 
+WHERE e.EmployeeID IN (SELECT o.SalesPersonID  FROM Sales.Orders o );
+
+
+
+-- with ANY | ALL
+SELECT 
+	e.EmployeeID ,
+	e.FirstName ,
+	e.Salary 
+FROM Sales.Employees e 
+WHERE e.Gender = 'M' AND e.Salary > ALL (SELECT e2.Salary FROM Sales.Employees e2 WHERE e2.Gender = 'F');
+
+
+
+-- NON-CORRELATED and CORRELATED sub query
+-- correlated sub query
+SELECT 
+*,
+(SELECT COUNT(*) FROM Sales.Orders o WHERE o.SalesPersonID = e.EmployeeID ) AS totalSales
+FROM Sales.Employees e ;
+
+-- correlated sub query EXIST
+SELECT 
+*,
+(SELECT COUNT(*) FROM Sales.Orders o WHERE o.SalesPersonID = e.EmployeeID ) AS totalSales
+FROM Sales.Employees e 
+WHERE EXISTS (SELECT 1 FROM Sales.Orders o WHERE o.SalesPersonID = e.EmployeeID );
+
+
+
 -- tasks:
 -- rank coustomers based on their total amount of sales
 SELECT 
@@ -111,3 +154,55 @@ JOIN (SELECT
 FROM Sales.Orders o 
 GROUP BY o.CustomerID ) AS t
 	ON t.CustomerID = c.CustomerID ;
+
+
+-- find the products that have a price higher than the average price of all products.
+SELECT 
+*
+FROM Sales.Products p 
+WHERE p.Price > (SELECT AVG(p2.Price ) FROM Sales.Products p2 );
+
+
+-- show the details of orders made by customers in germany
+SELECT 
+*
+FROM Sales.Orders o 
+WHERE o.CustomerID IN (SELECT c.CustomerID FROM Sales.Customers c WHERE c.Country = 'Germany');
+-- show the details of orders made by customers not in germany
+SELECT 
+*
+FROM Sales.Orders o 
+WHERE o.CustomerID IN (SELECT c.CustomerID FROM Sales.Customers c WHERE c.Country != 'Germany');
+
+
+-- Find female employees whose salaries are greater than the salaries of any male employees
+SELECT 
+	e.EmployeeID ,
+	e.FirstName ,
+	e.Salary 
+FROM Sales.Employees e 
+WHERE e.Gender = 'F' AND e.Salary > ANY (SELECT e2.Salary FROM Sales.Employees e2 WHERE e2.Gender = 'M');
+SELECT 
+	e.EmployeeID ,
+	e.FirstName ,
+	e.Salary 
+FROM Sales.Employees e 
+WHERE e.Gender = 'F' AND e.Salary > ALL (SELECT e2.Salary FROM Sales.Employees e2 WHERE e2.Gender = 'M');
+
+
+-- Show all customer details and find the total orders of each customer
+SELECT 
+*,
+(SELECT COUNT(*) FROM Sales.Orders o WHERE o.CustomerID = c.CustomerID  ) AS totalSales
+FROM Sales.Customers c ;
+
+-- Show the details of orders made by customers in Germany
+SELECT 
+*
+FROM Sales.Orders o 
+WHERE EXISTS (SELECT 1 FROM Sales.Customers c WHERE c.Country = 'Germany' AND o.CustomerID = c.CustomerID );
+-- Show the details of orders made by customers not in Germany
+SELECT 
+*
+FROM Sales.Orders o 
+WHERE NOT EXISTS (SELECT 1 FROM Sales.Customers c WHERE c.Country = 'Germany' AND o.CustomerID = c.CustomerID );
