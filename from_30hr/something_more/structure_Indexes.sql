bd810acdc9af209e262e2077fdaf1f83b99f7d5b
md-habibullah-99 <habibullahhisam@gmail.com>
2026-07-24 12:51:47 +0600

    Learn about structured index.

---
+++ from_30hr/something_more/structure_Indexes.sql
@@ -0,0 +1,56 @@
+
+USE SalesDB;
+--------------------------      INDEXES       --------------------------
+
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+---------------------------  structure (clustered index and non-cluster index)  --------------
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- syntext:
+-- CREATE [CLUSTERED | NONCLUSTERED] INDEX index_name ON table_name (column1, column2, .............)
+
+
+SELECT *
+FROM Sales.DBCustomers dbc 
+WHERE dbc.CustomerID = 1; -- heap cluster
+
+-- CLUSTERED
+CREATE CLUSTERED INDEX idx_DBCustomers_CustomerID
+ON Sales.DBCustomers (CustomerID);
+
+SELECT *
+FROM Sales.DBCustomers dbc 
+WHERE dbc.CustomerID = 1;
+
+
+-- NONCLUSTERED
+CREATE NONCLUSTERED INDEX idx_DBCustomers_LastName
+ON Sales.DBCustomers (LastName);
+
+SELECT *
+FROM Sales.DBCustomers dbc 
+WHERE dbc.LastName = 'Brown';
+
+CREATE INDEX idx_DBCustomers_FirstName
+ON Sales.DBCustomers (FirstName);
+
+SELECT *
+FROM Sales.DBCustomers dbc 
+WHERE dbc.FirstName = 'Anna';
+
+-- drop them
+DROP INDEX idx_DBCustomers_CustomerID ON Sales.DBCustomers;
+DROP INDEX idx_DBCustomers_FirstName ON Sales.DBCustomers;
+DROP INDEX idx_DBCustomers_LastName ON Sales.DBCustomers;
+
+
+---------------------- composite index -------------
+SELECT *
+FROM Sales.DBCustomers 
+WHERE Country = 'USA' AND Score > 500; -- same order of collumn check as index
+
+SELECT *
+FROM Sales.DBCustomers 
+WHERE Country = 'USA'; -- it will also work as LEFTMOST PREFIX RULE
+
+CREATE INDEX idx_DBCustomers_CountryScore
+ON Sales.DBCustomers (Country, Score); -- same order of column as select where statement
