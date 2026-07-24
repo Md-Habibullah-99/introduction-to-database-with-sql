e174e6fc2f45a14fa7cbe279767455371f960e36
md-habibullah-99 <habibullahhisam@gmail.com>
2026-07-25 00:28:23 +0600

    Learn about Column store indexs.

--- from_30hr/something_more/Indexes.sql
+++ from_30hr/something_more/Indexes.sql
@@ -1,4 +1,3 @@
-
 USE SalesDB;
 --------------------------      INDEXES       --------------------------
 
@@ -54,3 +53,25 @@ WHERE Country = 'USA'; -- it will also work as LEFTMOST PREFIX RULE
 
 CREATE INDEX idx_DBCustomers_CountryScore
 ON Sales.DBCustomers (Country, Score); -- same order of column as select where statement
+
+
+
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+---------------------------  storage (Rowstore index Columnstore index)  ------------------------------
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+-- columnstore index:
+-- syntex: 
+-- CREATE [CLUSTERED | NONCLUSTERED] [COLUMNSTORE] INDEX idnex_name ON table_name (column1, column2, ..........)
+-- note : default is rowstore, and for clustered columnstore index there is no column name specification is allowed 
+
+CREATE CLUSTERED COLUMNSTORE INDEX idx_DBCustomers_CS
+ON Sales.DBCustomers;
+
+
+CREATE NONCLUSTERED COLUMNSTORE INDEX idx_DBCustomers_CS_FirstName
+ON Sales.DBCustomers (FirstName);
+
+
+DROP INDEX idx_DBCustomers_CS ON Sales.DBCustomers;
+DROP INDEX idx_DBCustomers_CS_FirstName ON Sales.DBCustomers;
