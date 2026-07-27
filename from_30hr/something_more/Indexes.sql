5e4ca9183ae0d7aa18deb7f529d6b96095066259
md-habibullah-99 <habibullahhisam@gmail.com>
2026-07-27 18:24:51 +0600

    Learn about index functions.

--- from_30hr/something_more/Indexes.sql
+++ from_30hr/something_more/Indexes.sql
@@ -75,3 +75,33 @@ ON Sales.DBCustomers (FirstName);
 
 DROP INDEX idx_DBCustomers_CS ON Sales.DBCustomers;
 DROP INDEX idx_DBCustomers_CS_FirstName ON Sales.DBCustomers;
+
+
+
+
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+---------------------------  functions (unique index and filtered index)  ------------------------------
+--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+-- unique index syntex:
+-- CREATE [UNIQUE] [CLUSTERED | NONCLUSTERED] [COLUMNSTORE] INDEX idex_name 
+-- ON table_name (column1, column2, .....)
+-- note: in unique index no duplicates is allowed
+
+SELECT * FROM Sales.Products p ;
+
+CREATE UNIQUE NONCLUSTERED INDEX idx_products_product
+ON Sales.Products (product );
+
+
+-- filtered index syntex:
+-- CREATE [UNIQUE] [CLUSTERED | NONCLUSTERED] [COLUMNSTORE] INDEX idex_name 
+-- ON table_name (column1, column2, .....)
+-- WHERE [condition]
+
+SELECT * FROM Sales.Customers c 
+WHERE c.Country = 'USA';
+
+CREATE NONCLUSTERED INDEX idx_customers_country
+ON Sales.Customers (Country)
+WHERE Country = 'USA';
